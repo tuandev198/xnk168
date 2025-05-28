eeabbcf5b5d551cadb0685ae275c3b8601453936
tuandev198 <tuandev198@gmail.com>
2025-05-28 16:51:50 +0700

    add

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
