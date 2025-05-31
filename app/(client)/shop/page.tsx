07162af327bf748537ccba61ee7eafdd99b1c382
tuandev198 <tuandev198@gmail.com>
2025-05-31 11:07:41 +0700

    add

--- app/(client)/shop/page.tsx
+++ app/(client)/shop/page.tsx
@@ -1,10 +1,13 @@
 import Shop from "@/components/Shop";
 import React from "react";
+import { Suspense } from "react";
 
 const ShopPage = async () => {
   return (
     <div className="bg-white">
+      <Suspense fallback={<div>Loading...</div>}>
       <Shop />
+      </Suspense>
     </div>
   );
 };
