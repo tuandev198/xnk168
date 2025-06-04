e6395cbe711ba4f1ad98e457e8b3cadf63b0fcc8
tuandev198 <tuandev198@gmail.com>
2025-06-04 10:20:24 +0700

    add

--- components/ContactDetails.jsx
+++ components/ContactDetails.jsx
@@ -23,7 +23,7 @@ const ContactDetails = () => {
         <dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
           {[
             ["Careers", "reactjsbd.com"],
-            ["Press", "noorjsdivs@gmail.com"],
+            ["Press", "xnk@gmail.com"],
           ].map(([label, email]) => (
             <div key={email}>
               <dt className="font-semibold text-neutral-950">{label}</dt>
