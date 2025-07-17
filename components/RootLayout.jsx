4f3bd6342b3f6ac6c8f77ce5aea5881af4a2e6a9
tuandev198 <tuandev198@gmail.com>
2025-07-17 09:37:49 +0700

    aa

--- components/RootLayout.jsx
+++ components/RootLayout.jsx
@@ -182,7 +182,6 @@ const RootLayoutInner = ({ children }) => {
 
             <motion.div
                 layout
-                style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
                 className="relative flex flex-auto overflow-hidden bg-white "
             >
                 <motion.div layout className="relative isolate flex w-full flex-col">
