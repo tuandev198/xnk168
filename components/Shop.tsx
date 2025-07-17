fcd5f6e54534048d9447c5d209def6da8b69a3ed
tuandev198 <tuandev198@gmail.com>
2025-07-17 08:45:35 +0700

    aa

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -12,7 +12,7 @@ import { Loader2, List, Grid2X2 } from "lucide-react";
 const Shop = () => {
   const searchParams = useSearchParams();
   const brandParams = searchParams?.get("brand");
-  const categoryParams = searchParams?.get("category");
+
   const [products, setProducts] = useState([]);
   const [categories, setProductss] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -21,7 +21,6 @@ const Shop = () => {
 
   const [viewMode, setViewMode] = useState<"grid" | "list">("grid");
   const [itemsPerPage, setItemsPerPage] = useState<number>(100);
-  const [sortOption, setSortOption] = useState<string>("featured");
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
   const [selectedBrand, setSelectedBrand] = useState<string | null>(
     brandParams || null
@@ -99,37 +98,27 @@ const Shop = () => {
     fetchProducts();
   }, [selectedCategory, selectedBrand, selectedPrice]);
 
-
   return (
     <div className="border-t">
       <Container className="mt-5">
         <div className="sticky top-0 z-10 mb-5">
           <div className="flex items-center justify-between flex-wrap gap-3 pb-3">
             <div className="flex gap-2">
-              <button onClick={() => setViewMode("grid")} className={`p-1 border rounded-full w-9 h-9 flex items-center justify-center ${viewMode === 'grid' ? 'bg-orange-100 text-orange-800 border-orange-800' : ''}`}>
-                <Grid2X2 className="w-5 h-5" />
-              </button>
-              <button onClick={() => setViewMode("list")} className={`p-1 border rounded-full w-9 h-9 flex items-center justify-center ${viewMode === 'list' ? 'bg-orange-100 text-orange-800 border-orange-800' : ''}`}>
-                <List className="w-5 h-5" />
-              </button>
+              <h1 className="text-center text-5xl font-bold my-6">
+  SẢN PHẨM{" "}
+  {selectedCategory === "driedfoods"
+    ? "HÀNG KHÔ"
+    : selectedCategory === "frozenfoods"
+    ? "HÀNG ĐÔNG LẠNH"
+    : selectedCategory === "householdproducts"
+    ? "ĐỒ GIA DỤNG"
+    : selectedCategory === "instantfoods"
+    ? "THỰC PHẨM ĂN LIỀN"
+    : "TẤT CẢ"}
+</h1>
             </div>
             <div className="flex gap-4 items-center">
-              <div className="flex items-center gap-1">
-                <span>Paginate by</span>
-                <select value={itemsPerPage} onChange={(e) => setItemsPerPage(Number(e.target.value))} className="border px-2 py-1 rounded-full border-orange-800 text-orange-800">
-                  {[100].map((num) => (
-                    <option key={num} value={num}>{num}</option>
-                  ))}
-                </select>
-              </div>
-              <div className="flex items-center gap-1">
-                <span>Sort by</span>
-                <select value={sortOption} onChange={(e) => setSortOption(e.target.value)} className="border px-2 py-1 rounded-full border-orange-800 text-orange-800">
-                  <option value="featured">Featured</option>
-                  <option value="low-to-high">Price: Low to High</option>
-                  <option value="high-to-low">Price: High to Low</option>
-                </select>
-              </div>
+                
             </div>
             {(selectedCategory || selectedBrand || selectedPrice) && (
               <button
@@ -140,7 +129,7 @@ const Shop = () => {
                 }}
                 className="text-orange-800 underline text-sm mt-2 font-medium hover:text-red-700"
               >
-                Reset Filters
+                Tất cả
               </button>
             )}
           </div>
